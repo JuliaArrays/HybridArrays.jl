504afe9f5ea899d094164cdeeef8960c00b7c770
Mateusz Baran <mateuszbaran89@gmail.com>
2019-11-03 18:21:46 +0100

    zero() and update to StaticArrays 0.12.0

--- src/utils.jl
+++ src/utils.jl
@@ -7,19 +7,3 @@ Converts a size given by `Tuple{N, M, ...}` into a tuple `(N, M, ...)`.
 Base.@pure function size_to_tuple(::Type{S}) where S<:Tuple
     return tuple(S.parameters...)
 end
-
-
-# FOR COMPATIBILITY WITH STATIC ARRAYS 0.11.0 -- REMOVE DURING UPDATE TO THE NEXT VERSION
-
-# Base.strides is intentionally not defined for SArray, see PR #658 for discussion
-Base.strides(a::MArray) = Base.size_to_strides(1, size(a)...)
-Base.strides(a::SizedArray) = strides(a.data)
-
-Base.unsafe_view(A::AbstractArray, i1::StaticArrays.StaticIndexing, indices::StaticArrays.StaticIndexing...) = Base.unsafe_view(A, StaticArrays.unwrap(i1), map(StaticArrays.unwrap, indices)...)
-
-# Views of views need a new method for Base.SubArray because storing indices
-# wrapped in StaticIndexing in field indices of SubArray causes all sorts of problems.
-# Additionally, in some cases the SubArray constructor may be called directly
-# instead of unsafe_view so we need this method too (Base._maybe_reindex
-# is a good example)
-Base.SubArray(A::AbstractArray, indices::NTuple{<:Any,StaticArrays.StaticIndexing}) = Base.SubArray(A, map(StaticArrays.unwrap, indices))
--- test/arraymath.jl
+++ test/arraymath.jl
@@ -10,6 +10,15 @@ using StaticArrays, HybridArrays, Test, LinearAlgebra
         @test (@inferred one(Mv)) === @SMatrix [1 0; 0 1]
     end
 
+    @testset "zero()" begin
+        M = HybridMatrix{2, StaticArrays.Dynamic()}([1 2; 4 5])
+        @test (@inferred zero(M)) == @SMatrix [0 0; 0 0]
+        @test isa(one(M), HybridMatrix{2,StaticArrays.Dynamic(),Int})
+
+        Mv = view(M, :, SOneTo(2))
+        @test (@inferred zero(Mv)) === @SMatrix [0 0; 0 0]
+    end
+
     @testset "fill!()" begin
         M = HybridMatrix{2, StaticArrays.Dynamic(), Float64}([1 2; 4 5])
         fill!(M, 3)
