b4a50588d1ba6c8df3abeee536b707aefb938c39
Mateusz Baran <mateuszbaran89@gmail.com>
2022-03-18 22:13:10 +0100

    Fix HybridArrays after combine_sizes rework of StaticArrays (#51)
    * Fix HybridArrays after combine_sizes rework of StaticArrays
    
    * update CI
    
    * add EllipsisNotation compat bound
    
    * remove EllipsisNotation compat?
    
    * Require Julia 1.5

--- src/broadcast.jl
+++ src/broadcast.jl
@@ -2,6 +2,46 @@
 import Base.Broadcast: BroadcastStyle
 using Base.Broadcast: AbstractArrayStyle, Broadcasted, DefaultArrayStyle
 
+# combine_sizes moved from StaticArrays after https://github.com/JuliaArrays/StaticArrays.jl/pull/1008
+# see also https://github.com/JuliaArrays/HybridArrays.jl/issues/50
+@generated function combine_sizes(s::Tuple{Vararg{Size}})
+    sizes = [sz.parameters[1] for sz ∈ s.parameters]
+    ndims = 0
+    for i = 1:length(sizes)
+        ndims = max(ndims, length(sizes[i]))
+    end
+    newsize = StaticArrays.StaticDimension[Dynamic() for _ = 1 : ndims]
+    for i = 1:length(sizes)
+        s = sizes[i]
+        for j = 1:length(s)
+            if s[j] isa Dynamic
+                continue
+            elseif newsize[j] isa Dynamic || newsize[j] == 1
+                newsize[j] = s[j]
+            elseif newsize[j] ≠ s[j] && s[j] ≠ 1
+                throw(DimensionMismatch("Tried to broadcast on inputs sized $sizes"))
+            end
+        end
+    end
+    quote
+        Base.@_inline_meta
+        Size($(tuple(newsize...)))
+    end
+end
+
+function broadcasted_index(oldsize, newindex)
+    index = ones(Int, length(oldsize))
+    for i = 1:length(oldsize)
+        if oldsize[i] != 1
+            index[i] = newindex[i]
+        end
+    end
+    return LinearIndices(oldsize)[index...]
+end
+
+scalar_getindex(x) = x
+scalar_getindex(x::Ref) = x[]
+
 # Add a new BroadcastStyle for StaticArrays, derived from AbstractArrayStyle
 # A constructor that changes the style parameter N (array dimension) is also required
 struct HybridArrayStyle{N} <: AbstractArrayStyle{N} end
@@ -22,7 +62,7 @@ BroadcastStyle(::HybridArray{M}, ::StaticArrays.StaticArrayStyle{0}) where {M} =
 @inline function Base.copy(B::Broadcasted{HybridArrayStyle{M}}) where M
     flat = Broadcast.flatten(B); as = flat.args; f = flat.f
     argsizes = StaticArrays.broadcast_sizes(as...)
-    destsize = StaticArrays.combine_sizes(argsizes)
+    destsize = combine_sizes(argsizes)
     if Length(destsize) === Length{StaticArrays.Dynamic()}()
         # destination dimension cannot be determined statically; fall back to generic broadcast
         return HybridArray{StaticArrays.size_tuple(destsize)}(copy(convert(Broadcasted{DefaultArrayStyle{M}}, B)))
@@ -35,7 +75,7 @@ end
 @inline function _copyto!(dest, B::Broadcasted{HybridArrayStyle{M}}) where M
     flat = Broadcast.flatten(B); as = flat.args; f = flat.f
     argsizes = StaticArrays.broadcast_sizes(as...)
-    destsize = StaticArrays.combine_sizes((Size(dest), argsizes...))
+    destsize = combine_sizes((Size(dest), argsizes...))
     if Length(destsize) === Length{StaticArrays.Dynamic()}()
         # destination dimension cannot be determined statically; fall back to generic broadcast!
         return copyto!(dest, convert(Broadcasted{DefaultArrayStyle{M}}, B))
@@ -68,11 +108,11 @@ end
 
     make_expr(i) = begin
         if !(a[i] <: AbstractArray)
-            return :(StaticArrays.scalar_getindex(a[$i]))
+            return :(scalar_getindex(a[$i]))
         elseif hasdynamic(Tuple{sizes[i]...})
             return :(a[$i][$(current_ind...)])
         else
-            :(a[$i][$(StaticArrays.broadcasted_index(sizes[i], current_ind))])
+            :(a[$i][$(broadcasted_index(sizes[i], current_ind))])
         end
     end
 
